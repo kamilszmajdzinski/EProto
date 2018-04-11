8456ee53b6fd2c997315708ac4864bb83b109875
kamilszmajdzinski <user1@scm.com>
2018-04-12 00:57:18 +0200

    first endpoint

--- src/main/java/Main/Service.java
+++ src/main/java/Main/Service.java
@@ -8,6 +8,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/")
 public class Service {
@@ -17,8 +18,10 @@ public class Service {
     @GET
     @Path("/students")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Student getStudentByIndex(@PathParam("index") int index){
+
+    public List<Student> getStudents(){
         return dataBase.getStudents();
     }
+
 
 }
